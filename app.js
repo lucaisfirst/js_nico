f8b16ef6947393d5cb9424c5c1ca9b1a7c54ac43
김하진 <knj9417@gmail.com>
2021-07-28 22:31:08 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+// const a = 5;
+// const b = 2;
+// let myName = "hajin";
+
+// console.log(a+b);
+// console.log(a*b);
+// console.log(a/b);
+// console.log("hello " + myName);
+
+// myName = "luca"
+// console.log("And your name is " + myName);
+
+// const amIFat = null;
+// let something;
+// console.log(something, amIFat);
+
+
+// const daysOfWeek = ["mon", "tue", "wed", "thu", "fri", "sat"];
+
+// //Get Item from Array 
+// console.log(daysOfWeek);
+
+// // Add one more day to the array
+// daysOfWeek.push("sun");
+// console.log(daysOfWeek);
+
