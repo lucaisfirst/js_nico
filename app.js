5c40e977473dc4487180dc7e9df22bf4aa122cbd
김하진 <knj9417@gmail.com>
2021-07-29 22:51:40 +0900

    understand function

--- app.js
+++ app.js
@@ -14,10 +14,9 @@
 // let something;
 // console.log(something, amIFat);
 
-
 // const daysOfWeek = ["mon", "tue", "wed", "thu", "fri", "sat"];
 
-// //Get Item from Array 
+// //Get Item from Array
 // console.log(daysOfWeek);
 
 // // Add one more day to the array
@@ -34,10 +33,29 @@
 // player.potints = player.points + 15;
 // console.log(player);
 
+//  function sayHello(nameOfPerson, age) {
+//      console.log("Hello my name is " + nameOfPerson + "and I'm " + age);
+//  }
+// sayHello("jin", 20);
+// sayHello("luca", 21);
+// sayHello("suzu", 19);
 
- function sayHello(nameOfPerson, age) {
-     console.log("Hello my name is " + nameOfPerson + "and I'm " + age);
- }
-sayHello("jin", 20);
-sayHello("luca", 21);
-sayHello("suzu", 19);
+// function plus(firstNumber, secondNumber){
+//     console.log(firstNumber + secondNumber);
+// }
+// function divide(a, b){
+//     console.log(a / b);
+// }
+
+// plus(8, 60);
+// divide(100, 23);
+
+const player = {
+  name: "luca",
+  sayHello: function (otherPersonsName) {
+    console.log("hello " + otherPersonsName + " nice to meet you!");
+  },
+};
+
+console.log(player.name);
+player.sayHello("luca");
