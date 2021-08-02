f6489f18d91440188f66ce9a6f0b68323ee5542c
김하진 <knj9417@gmail.com>
2021-08-02 22:52:03 +0900

    Searching for Elements

--- app.js
+++ app.js
@@ -210,9 +210,24 @@
 
 //js에서 html의 요소들을 변경하고 바꿀 수 있다는 것이다.
 // document.title = "Hello! From JS!";
-const title = document.getElementById("title");
+// const title = document.getElementById("title");
 
-title.innerText = "Got you!";
+// title.innerText = "Got you!";
 
-console.log(title.id);
-console.log(title.className);
+// console.log(title.id);
+// console.log(title.className);
+
+// //searching for elements
+// const title = document.querySelector(".hello h1");
+// title.innerText = "hello";
+// // 정리
+// // - getElementsByClassName() : 많은 element를 가져올때 씀(array를 반환)
+// // - getElementsByTagName() : name을 할당할 수 있음(array를 반환)
+// // - querySelector : element를 CSS selector방식으로 검색할 수 있음 (ex. h1:first-child)
+// // 단 하나의 element를 return해줌
+// // ⇒ hello란 class 내부에 있는 h1을 가지고 올 수 있다(id도 가능함)
+// // - 첫번째 element만 가져옴
+// // - 조건에 맞는 세개 다 가져오고 싶으면 querySelectorAll
+// // ⇒ 세개의 h1이 들어있는 array를 가져다 줌
+// // - querySelector("#hello); 와 getElementById("hello"); 는 같은 일을 하는 것임
+// // 하지만 후자는 하위요소 가져오는 것을 못하므로 전자만 쓸거다
