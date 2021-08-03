93e3526e947f18d8ed51b707b01f8c50eeb686f7
김하진 <knj9417@gmail.com>
2021-08-03 12:54:03 +0900

    Events 2

--- app.js
+++ app.js
@@ -235,8 +235,19 @@
 //Events detect click
 const title = document.querySelector(".hello:first-child h1");
 
+console.dir(title);
+
 function handleTitleClick() {
   title.style.color = "blue";
 }
 
+function handleMouseEnter() {
+  title.innerText = "Mouse is here!";
+}
+
+function handleMouseLeave() {
+  title.innerText = "Mouse is gone!";
+}
 title.addEventListener("click", handleTitleClick);
+title.addEventListener("mouseenter", handleMouseEnter);
+title.addEventListener("mouseleave", handleMouseLeave);
